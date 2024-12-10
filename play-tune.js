646e90cb05707a7063d70a09852b8dd67efa9f67
sinan np <sinu007j@gmail.com>
2024-12-11 01:14:10 +0530

    add keyup option

--- play-tune.js
+++ play-tune.js
@@ -1,18 +1,38 @@
-document.querySelectorAll(".music-div").forEach(musicDiv => {
+const musicDivs = document.querySelectorAll(".music-div");
+
+// Add click functionality
+musicDivs.forEach(musicDiv => {
     musicDiv.addEventListener("click", function () {
-    
-        document.querySelectorAll(".music-div").forEach(div => {
-            div.classList.remove("music-div-click");
-        });
-
-      
-        this.classList.add("music-div-click");
-
-        
-        let musicPath = this.getAttribute("data-music");
-
-      
-        let audio = new Audio(musicPath);
-        audio.play();
+        playMusic(this);
     });
 });
+
+// Add keydown functionality
+document.addEventListener("keydown", function (event) {
+    const typedLetter = event.key.toUpperCase(); // Get the typed letter (case-insensitive)
+    
+    musicDivs.forEach(musicDiv => {
+        const letter = musicDiv.querySelector(".para").textContent.trim().toUpperCase();
+        if (letter === typedLetter) {
+            playMusic(musicDiv);
+        }
+    });
+});
+
+
+function playMusic(musicDiv) {
+   
+    musicDivs.forEach(div => {
+        div.classList.remove("music-div-click");
+    });
+
+   
+    musicDiv.classList.add("music-div-click");
+
+    
+    const musicPath = musicDiv.getAttribute("data-music");
+
+    
+    const audio = new Audio(musicPath);
+    audio.play();
+}
